bd110937b911a21ca6babf74bdf3cd370f99d9bb
once <once12@yandex.ru>
2019-12-02 23:31:55 +0300

    proceeding

---
+++ DocCardExample/src/webparts/docCardExample/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDocCardExampleWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DocCardExampleWebPartStrings' {
+  const strings: IDocCardExampleWebPartStrings;
+  export = strings;
+}
