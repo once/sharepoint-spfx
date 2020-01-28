cb035b85d704457a44d31cfbb64c07f4b25b9b08
once <once12@yandex.ru>
2020-01-29 00:17:41 +0300

    created global-message project

---
+++ global-message-extension/src/extensions/globalMessage/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IGlobalMessageApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'GlobalMessageApplicationCustomizerStrings' {
+  const strings: IGlobalMessageApplicationCustomizerStrings;
+  export = strings;
+}
