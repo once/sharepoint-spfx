3f0b4c9596a99a382612b627ad92c197394238a9
once <once12@yandex.ru>
2020-01-30 00:42:13 +0300

    started developing global message appCustomizer

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Workflow:
 Preview locally: gulp serve
 
 <b>My current position in tutorial:</b>
-https://docs.microsoft.com/en-us/sharepoint/dev/spfx/web-parts/guidance/migrate-script-editor-web-part-customizations
+https://www.sharepointnutsandbolts.com/2017/06/SPFx-Application-Customizer-Global-JS-Page-Header.html
+https://docs.microsoft.com/en-us/sharepoint/dev/spfx/extensions/get-started/using-page-placeholder-with-extensions
+
 
 <b>Reference:</b>
 
--- global-message-extension/src/extensions/globalMessage/GlobalMessageApplicationCustomizer.ts
+++ global-message-extension/src/extensions/globalMessage/GlobalMessageApplicationCustomizer.ts
@@ -1,11 +1,12 @@
 import { override } from '@microsoft/decorators';
 import { Log } from '@microsoft/sp-core-library';
 import {
-  BaseApplicationCustomizer
+  BaseApplicationCustomizer, PlaceholderContent, PlaceholderName
 } from '@microsoft/sp-application-base';
 import { Dialog } from '@microsoft/sp-dialog';
 
 import * as strings from 'GlobalMessageApplicationCustomizerStrings';
+import styles from './GlobalMessageApplicationCustomizer.module.scss';
 
 const LOG_SOURCE: string = 'GlobalMessageApplicationCustomizer';
 
@@ -16,7 +17,7 @@ const LOG_SOURCE: string = 'GlobalMessageApplicationCustomizer';
  */
 export interface IGlobalMessageApplicationCustomizerProperties {
   // This is an example; replace with your own property
-  testMessage: string;
+  TopMessage: string;
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
@@ -25,15 +26,26 @@ export default class GlobalMessageApplicationCustomizer
 
   @override
   public onInit(): Promise<void> {
+    
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+    
+    this.context.placeholderProvider.changedEvent.add(this, this.renderHeader);
+	
+    return Promise.resolve<void>();
+  }
 
-    let message: string = this.properties.testMessage;
-    if (!message) {
-      message = '(No properties were provided.)';
+  private renderHeader(): void {
+    
+    let topPlaceholder: PlaceholderContent = this.context.placeholderProvider.tryCreateContent(PlaceholderName.Top); 
+
+    if (topPlaceholder) {
+
+      topPlaceholder.domElement.innerHTML = `<div class="${styles.app}">
+      <div class="ms-bgColor-themeDark ms-fontColor-white ${styles.header}">
+        <i class="ms-Icon ms-Icon--Info" aria-hidden="true"></i>&nbsp; ${this.properties.TopMessage}
+      </div>
+    </div>`;
+
     }
-
-    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
-
-    return Promise.resolve();
   }
 }
